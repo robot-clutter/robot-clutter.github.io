3531009bc05d1abdafa76e6513abaf0deee3ad44
Theofanis prapavesis Semetzidis <thprapav@gmail.com>
2021-01-18 02:36:22 +0200

    maybe bibtex fix

--- components/publications/BibtexBox.tsx
+++ components/publications/BibtexBox.tsx
@@ -1,33 +1,27 @@
 import { ReactElement } from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-// import { dark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
-import { useTheme } from 'next-themes';
+import Highlight, { defaultProps } from 'prism-react-renderer';
+import theme from 'prism-react-renderer/themes/oceanicNext';
 
 function BibtexBox({ bib }: { bib?: string }): ReactElement {
-  const { resolvedTheme } = useTheme();
-
-  const isDarkMode = resolvedTheme === 'dark';
-
   return (
     <>
       {bib && (
         <>
           <div className="mt-5 prose prose-cyan text-gray-500 dark:text-gray-300 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
             <h3 className="text-gray-500 dark:text-gray-300">Bibtex</h3>
-            <SyntaxHighlighter
-              // styles={dark}
-              // useInlineStyles={false}
-              language="latex"
-              // className="p-0 m-0"
-              customStyle={{
-                padding: '16px',
-                margin: 0,
-                // background: isDarkMode ? '#fff' : '000',
-                // backgroundColor: isDarkMode ? '#fff' : '000',
-              }}
-            >
-              {bib}
-            </SyntaxHighlighter>
+            <Highlight {...defaultProps} code={bib} language="makefile" theme={theme}>
+              {({ className, style, tokens, getLineProps, getTokenProps }) => (
+                <pre className={className} style={style}>
+                  {tokens.map((line, i) => (
+                    <div {...getLineProps({ line, key: i })}>
+                      {line.map((token, key) => (
+                        <span {...getTokenProps({ token, key })} />
+                      ))}
+                    </div>
+                  ))}
+                </pre>
+              )}
+            </Highlight>
           </div>
         </>
       )}

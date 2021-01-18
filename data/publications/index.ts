8b4906ae5e9569f422735e0d426dfbddea87a834
Iason Sarantopoulos <iason@sarantop.com>
2021-01-18 13:58:00 +0200

    Remove again pages

--- data/publications/index.ts
+++ data/publications/index.ts
@@ -3,6 +3,6 @@ import DQN_PUB from './dqn';
 import SPLIT_DQN_PUB from './split-dqn';
 import MODULAR_RL_PUB from './modular-rl';
 
-const publications: PublicationInterface[] = [DQN_PUB, SPLIT_DQN_PUB, MODULAR_RL_PUB];
+const publications: PublicationInterface[] = [MODULAR_RL_PUB];
 
 export default publications;

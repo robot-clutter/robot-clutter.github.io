cd517b98cec7e19f535492e5e05ae811124a9ae8
Iason Sarantopoulos <iason@sarantop.com>
2021-12-03 13:31:11 +0200

    Fix the text in the intro

--- components/publications/Intro.tsx
+++ components/publications/Intro.tsx
@@ -14,8 +14,8 @@ function Intro(): ReactElement {
           </h2>
         </div>
         <div className="text-justify mt-10 text-xl">
-          In this project we aim to research and develop learning-based for manipulating objects in cluttered
-          environments. During this project a number of publication has been produced that are listed below.
+          In this project, we aim to research and develop learning-based methods for manipulating objects in cluttered
+          environments. During this project a number of publications have been produced and are listed below.
           Furthermore, the software developed for these methods is open source and can be found in our&nbsp;
           <span className="mt-6 font-medium text-cyan-600 dark:text-cyan-500 hover:underline">
             <Link href="https://github.com/robot-clutter">
